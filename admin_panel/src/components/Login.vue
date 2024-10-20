596cdaa8e53d7f0a030bf82e98b8a754b9f8ba19
Md Bulbul Hasan <bulbulhasan095@gmail.com>
2024-10-20 14:35:24 +0600

    2nd commit

--- admin_panel/src/components/Login.vue
+++ admin_panel/src/components/Login.vue
@@ -9,14 +9,36 @@ import {
   CardTitle,
 } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-// import {
-//   Select,
-//   SelectContent,
-//   SelectItem,
-//   SelectTrigger,
-//   SelectValue,
-// } from '@/components/ui/select'
+import {ref} from "vue";
+import axios from "axios";
+import {useRouter} from "vue-router";
+
+const isClicked = ref(false);
+const  router = useRouter();
+async function SubmitHandler(e) {
+  e.preventDefault();
+  isClicked.value = true;
+  // Input Binding
+  const formData = new FormData();
+  formData.append('username', e.target.name.value);
+  formData.append('password', e.target.password.value);
+  try {
+    const response = await axios.post(`https://dummyjson.com/auth/login`,formData, {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    console.log(response.data);
+    if (response.status === 200) {
+      isClicked.value = false;
+      await router.push('/AdminPanel');
+    }
+  }
+  catch (error) {
+    console.log(error);
+  }
+
+}
 </script>
 
 <template>
@@ -25,19 +47,21 @@ import { Label } from '@/components/ui/label'
       <CardTitle>Dashboard Login</CardTitle>
     </CardHeader>
     <CardContent>
-      <form>
+      <form @submit="SubmitHandler">
         <div class="grid items-center w-full gap-4">
-          <div class="flex flex-col space-y-1.5">
-            <Input type="text" id="name" placeholder="username" />
+          <div>
+            <Input type="text" id="name" name="name" placeholder="username" />
           </div>
           <div>
-            <Input type="password" id="name" placeholder="password"  />
+            <Input type="password" id="password" name="password" placeholder="password"  />
           </div>
+          <div class="flex w-full justify-end">
+            <Button class="w-[20%]" type="submit">{{isClicked ? "Loading" : "Login"}}</Button>
+          </div>
+
         </div>
+
       </form>
     </CardContent>
-    <CardFooter class="flex justify-end px-6 pb-6">
-      <Button>Deploy</Button>
-    </CardFooter>
   </Card>
 </template>/
