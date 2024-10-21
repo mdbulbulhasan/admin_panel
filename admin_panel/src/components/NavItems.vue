ac7ff31dec6e3981214f40dce10ec38aa430ccbb
Md Bulbul Hasan <bulbulhasan095@gmail.com>
2024-10-21 17:05:36 +0600

    Update NavItems.vue

--- admin_panel/src/components/NavItems.vue
+++ admin_panel/src/components/NavItems.vue
@@ -1,40 +1,57 @@
 <script setup>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+//Define an array of menu items
+const menuItems = [
+  {
+    icon: ['fas', 'clipboard-list'],
+    text: 'DASHBOARD',
+    route: '/AdminPanel',
+  },
+  {
+    icon: ['fas', 'table-list'],
+    text: 'TABLE LIST',
+    route: '/AdminPanel/TableList',
+  },
+  {
+    icon: ['fas', 'user'],
+    text: 'USER PROFILE',
+    route: '/AdminPanel',
+  },
+  {
+    icon: ['fas', 't'],
+    text: 'TYPOGRAPHY',
+    route: '/AdminPanel',
+  },
+  {
+    icon: ['fas', 'pencil'],
+    text: 'ICONS',
+    route: '/AdminPanel',
+  },
+  {
+    icon: ['fas', 'map'],
+    text: 'MAPS',
+    route: '/AdminPanel',
+  },
+  {
+    icon: ['fas', 'bell'],
+    text: 'NOTIFICATIONS',
+    route: '/AdminPanel',
+  },
+  {
+    icon: ['fas', 'arrow-up-from-bracket'],
+    text: 'UPDATE TO PRO',
+    route: '/AdminPanel',
+  },
+];
+
 </script>
 
 <template>
   <ul class="flex flex-col gap-5 bg-white shadow-lg shadow-black h-screen p-5 text-xl text-[rgba(0,0,0,0.75)] font-semibold">
-    <li class="h-14 flex gap-[0.8rem] pl-8 items-center justify-start bg-[rgba(0,0,0,0.04)] hover:bg-[rgba(0,0,0,0.3)] rounded group">
-      <font-awesome-icon :icon="['fas', 'clipboard-list']" class="text-3xl text-[rgba(0,0,0,0.35)] group-hover:text-[rgba(255,255,255,0.70)]" />
-      <router-link to="/AdminPanel" class="hover:font-semibold group-hover:text-white">DASHBOARD</router-link>
-    </li>
-    <li class="h-14 flex gap-[0.8rem] pl-8 items-center justify-start bg-[rgba(0,0,0,0.04)] hover:bg-[rgba(0,0,0,0.3)] rounded group">
-      <font-awesome-icon :icon="['fas', 'table-list']" class="text-3xl text-[rgba(0,0,0,0.35)] group-hover:text-[rgba(255,255,255,0.70)]" />
-      <router-link to="/AdminPanel/TableList" class="hover:font-semibold group-hover:text-white">TABLE LIST</router-link>
-    </li>
-    <li class="h-14 flex gap-[0.8rem] pl-8 items-center justify-start bg-[rgba(0,0,0,0.04)] hover:bg-[rgba(0,0,0,0.3)] rounded group">
-      <font-awesome-icon :icon="['fas', 'user']" class="text-3xl text-[rgba(0,0,0,0.35)] group-hover:text-[rgba(255,255,255,0.70)]" />
-      <a href="" class="hover:font-semibold group-hover:text-white">USER PROFILE</a>
-    </li>
-    <li class="h-14 flex gap-[0.8rem] pl-8 items-center justify-start bg-[rgba(0,0,0,0.04)] hover:bg-[rgba(0,0,0,0.3)] rounded group">
-      <font-awesome-icon :icon="['fas', 't']" class="text-3xl text-[rgba(0,0,0,0.35)] group-hover:text-[rgba(255,255,255,0.70)]" />
-      <a href="" class="hover:font-semibold group-hover:text-white">TYPOGRAPHY</a>
-    </li>
-    <li class="h-14 flex gap-[0.8rem] pl-8 items-center justify-start bg-[rgba(0,0,0,0.04)] hover:bg-[rgba(0,0,0,0.3)] rounded group">
-      <font-awesome-icon :icon="['fas', 'pencil']" class="text-3xl text-[rgba(0,0,0,0.35)] group-hover:text-[rgba(255,255,255,0.70)]" />
-      <a href="" class="hover:font-semibold group-hover:text-white">ICONS</a>
-    </li>
-    <li class="h-14 flex gap-[0.8rem] pl-8 items-center justify-start bg-[rgba(0,0,0,0.04)] hover:bg-[rgba(0,0,0,0.3)] rounded group">
-      <font-awesome-icon :icon="['fas', 'map']" class="text-3xl text-[rgba(0,0,0,0.35)] group-hover:text-[rgba(255,255,255,0.70)]" />
-      <a href="" class="hover:font-semibold group-hover:text-white">MAPS</a>
-    </li>
-    <li class="h-14  flex gap-[0.8rem] pl-8 items-center justify-start bg-[rgba(0,0,0,0.04)] hover:bg-[rgba(0,0,0,0.3)] rounded group">
-      <font-awesome-icon :icon="['fas', 'bell']" class="text-3xl text-[rgba(0,0,0,0.35)] group-hover:text-[rgba(255,255,255,0.70)]" />
-      <a href="" class="hover:font-semibold group-hover:text-white">NOTIFICATIONS</a>
-    </li>
-    <li class="h-14 flex gap-[0.8rem] pl-8 items-center justify-start bg-[rgba(0,0,0,0.04)] hover:bg-[rgba(0,0,0,0.3)] rounded group">
-      <font-awesome-icon :icon="['fas', 'arrow-up-from-bracket']" class="text-3xl text-[rgba(0,0,0,0.35)] group-hover:text-[rgba(255,255,255,0.70)]" />
-      <a href="" class="hover:font-semibold group-hover:text-white">UPGRADE TO PRO</a>
+    <li v-for="item in menuItems" :key="item.text" class="h-14 flex gap-[0.8rem] pl-8 items-center justify-start bg-[rgba(0,0,0,0.04)] hover:bg-[rgba(0,0,0,0.3)] rounded group">
+      <font-awesome-icon :icon="item.icon" class="text-3xl text-[rgba(0,0,0,0.35)] group-hover:text-[rgba(255,255,255,0.70)]" />
+      <router-link :to="item.route" class="hover:font-semibold group-hover:text-white">{{ item.text }}</router-link>
     </li>
   </ul>
 </template>
