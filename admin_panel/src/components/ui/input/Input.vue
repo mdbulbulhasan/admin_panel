40831ecec26fef8f4124efba5bf9b36644b91bcc
Md Bulbul Hasan <bulbulhasan095@gmail.com>
2024-10-21 17:05:41 +0600

    Update Input.vue

--- admin_panel/src/components/ui/input/Input.vue
+++ admin_panel/src/components/ui/input/Input.vue
@@ -21,7 +21,7 @@ const modelValue = useVModel(props, 'modelValue', emits, {
     v-model="modelValue"
     :class="
       cn(
-        'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        'flex h-10 w-full rounded-md border-none bg-background px-3 py-2 text-l ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-left placeholder:text-muted-foreground focus:outline-none focus:ring-0 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50',
         props.class,
       )
     "
